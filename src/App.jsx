db42583ed2b37defff164841707a0a0895bcc114
fabricioodsousa <fabricioodsousa1@gmail.com>
2025-06-26 21:17:13 -0300

    finalizando estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h1>Buscar CEP</h1>
+
+        <div className='containerInput'>
+          <input className='inputCep' type="text" placeholder='Digite o CEP' />
+          <button className='buttonCep'>Buscar</button>
+        </div>
+
+        <div className='containerInfo'>
+          <h2>CEP: </h2>
+          <p>Rua:</p>
+          <p>Complemento:</p>
+          <p>Bairro:</p>
+          <p>Localidade:</p>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
